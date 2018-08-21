447d3421136853b1dde542dab45a2071a37290e4
mariaBarczyk <barczyk.maria92@gmail.com>
2018-08-21 11:17:00 +0200

    Create recipe model

---
+++ src/main/java/com/codecool/krk/model/Recipe.java
@@ -0,0 +1,84 @@
+package com.codecool.krk.model;
+
+import java.util.List;
+
+public class Recipe {
+
+    private long id;
+    private String name;
+    private User author;
+    private List<Ingredient> ingredients;
+    private String instruction;
+    private List<Review> reviews;
+    private Category category;
+
+    public Recipe() {
+
+    }
+
+    public Recipe(long id, String name, User author, List<Ingredient> ingredients, String instruction, List<Review> reviews, Category category) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.ingredients = ingredients;
+        this.instruction = instruction;
+        this.reviews = reviews;
+        this.category = category;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public String getInstruction() {
+        return instruction;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public void setInstruction(String instruction) {
+        this.instruction = instruction;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
