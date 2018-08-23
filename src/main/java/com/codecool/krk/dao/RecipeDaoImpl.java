6d78112605cf52e53e79825373313ef41186dff3
Joanna Baran <joannabaran56@gmail.com>
2018-08-23 23:47:08 +0200

    Add responding with bad request when GET to inexistent recipe

--- src/main/java/com/codecool/krk/dao/RecipeDaoImpl.java
+++ src/main/java/com/codecool/krk/dao/RecipeDaoImpl.java
@@ -19,10 +19,12 @@ public class RecipeDaoImpl implements RecipeDao {
         try {
             em.getTransaction().begin();
             recipe = em.find(Recipe.class, id);
-            Hibernate.initialize(recipe.getIngredients());
-            Hibernate.initialize(recipe.getReviews());
+            if (recipe != null) {
+                Hibernate.initialize(recipe.getIngredients());
+                Hibernate.initialize(recipe.getReviews());
+            }
             em.getTransaction().commit();
-        } catch (HibernateException e) {
+        } catch (HibernateException | IllegalArgumentException e) {
             em.getTransaction().rollback();
             e.printStackTrace();
         } finally {
--- src/main/java/com/codecool/krk/servlet/SingleRecipeServlet.java
+++ src/main/java/com/codecool/krk/servlet/SingleRecipeServlet.java
@@ -3,6 +3,7 @@ package com.codecool.krk.servlet;
 import com.codecool.krk.dao.RecipeDao;
 import com.codecool.krk.dao.RecipeDaoImpl;
 import com.codecool.krk.model.Recipe;
+import com.codecool.krk.util.UriParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.servlet.ServletException;
@@ -12,7 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/recipes/*")
+@WebServlet() //"/recipes/*")
 public class SingleRecipeServlet extends HttpServlet {
 
     RecipeDao recipeDao = new RecipeDaoImpl();
@@ -21,19 +22,19 @@ public class SingleRecipeServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("application/json");
 
-        String uri = req.getRequestURI();
-        String[] splitUri = uri.split("/");
-        long id = Long.parseLong(splitUri[2]);
+        long id = UriParser.extractIdFromUri(req.getRequestURI());
 
         Recipe recipe = recipeDao.loadRecipe(id);
+        if (recipe != null) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String recipeJson = objectMapper.writeValueAsString(recipe);
+            resp.getWriter().print(recipeJson);
+        } else {
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        String recipeJson = objectMapper.writeValueAsString(recipe);
-
-        resp.getWriter().print(recipeJson);
     }
-
-
+    
     @Override
     protected void doDelete(HttpServletRequest request, HttpServletResponse response) {
         String idPart = request.getPathInfo();
