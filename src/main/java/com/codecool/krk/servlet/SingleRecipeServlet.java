5b0bb7efc1966f0777d8943b1bf2a99b71d929db
Katarzyna Drobniewicz <kasia.drobniewicz@gmail.com>
2018-08-23 11:50:41 +0200

    Add doGet method in SingleRecipeServlet file

--- src/main/java/com/codecool/krk/servlet/SingleRecipeServlet.java
+++ src/main/java/com/codecool/krk/servlet/SingleRecipeServlet.java
@@ -1,6 +1,39 @@
 package com.codecool.krk.servlet;
 
-import javax.servlet.http.HttpServlet;
+import com.codecool.krk.dao.RecipeDao;
+import com.codecool.krk.dao.RecipeDaoImpl;
+import com.codecool.krk.model.Recipe;
+import com.codecool.krk.model.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/recipes/*")
 public class SingleRecipeServlet extends HttpServlet {
+
+    RecipeDao recipeDao = new RecipeDaoImpl();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+
+        String uri = req.getRequestURI();
+        String [] splitUri = uri.split("/");
+        long id = Long.parseLong(splitUri[2]);
+
+        Recipe recipe = recipeDao.loadRecipe(id);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String recipeJson = objectMapper.writeValueAsString(recipe);
+
+        resp.getWriter().print(recipeJson);
+
+
+
+    }
 }
