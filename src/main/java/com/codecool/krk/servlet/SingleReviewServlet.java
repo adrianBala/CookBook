c292e983c4c519c5e5d68a8617338ebd74e71ced
Adrian Bala <aadrianbala@gmail.com>
2018-08-23 21:57:55 +0200

    Add doDelete method and refactor doPost method in SingleReviewServlet class

--- src/main/java/com/codecool/krk/servlet/SingleReviewServlet.java
+++ src/main/java/com/codecool/krk/servlet/SingleReviewServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/recipes/*")
+@WebServlet(urlPatterns = {"/recipe/*", "/reviews/*"})
 public class SingleReviewServlet extends HttpServlet {
 
     ReviewDao reviewDao = new ReviewDaoImpl();
@@ -24,22 +24,36 @@ public class SingleReviewServlet extends HttpServlet {
         String id = path.substring(path.indexOf("/") + 1, path.indexOf("/reviews"));
         System.out.println(id);
         RecipeDao recipeDao = new RecipeDaoImpl();
-        try {
-            Recipe recipe = recipeDao.loadRecipe(Long.parseLong(id));
+        Recipe recipe = recipeDao.loadRecipe(Long.parseLong(id));
+        if(recipe != null) {
             review.setRecipe(recipe);
             reviewDao.saveNewReview(review);
             resp.setStatus(HttpServletResponse.SC_OK);
-        } catch(NullPointerException e) {
+        } else {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+
+
+    }
+
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String id = req.getPathInfo().replaceFirst("/", "");
+        boolean isRemoved = reviewDao.removeReview(Long.parseLong(id));
+        System.out.println(id + isRemoved);
+        if(isRemoved) {
+            resp.setStatus(HttpServletResponse.SC_OK);
+        } else {
             resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
         }
 
     }
 
     private Review createReview(HttpServletRequest req) {
+
         int rating = Integer.parseInt(req.getParameter("rating"));
         String opinion = req.getParameter("opinion");
         String author = req.getParameter("author");
         return new Review(rating, opinion, author);
-
     }
 }
