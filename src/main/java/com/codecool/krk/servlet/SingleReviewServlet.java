987bdd4894a20b3cb60d8fbf56d93e2c058fca97
Adrian Bala <aadrianbala@gmail.com>
2018-08-24 10:20:49 +0200

    Remove doPost method and add doPut method in SingleReviewServlet class

--- src/main/java/com/codecool/krk/servlet/SingleReviewServlet.java
+++ src/main/java/com/codecool/krk/servlet/SingleReviewServlet.java
@@ -1,40 +1,24 @@
 package com.codecool.krk.servlet;
 
 import com.codecool.krk.dao.*;
-import com.codecool.krk.model.Recipe;
-import com.codecool.krk.model.Review;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
 
-@WebServlet(urlPatterns = {"/recipe/*", "/reviews/*"})
+@WebServlet(urlPatterns = {"/reviews/*"})
 public class SingleReviewServlet extends HttpServlet {
 
     ReviewDao reviewDao = new ReviewDaoImpl();
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        Review review = createReview(req);
-        String path = req.getPathInfo();
-        String id = path.substring(path.indexOf("/") + 1, path.indexOf("/reviews"));
-        System.out.println(id);
-        RecipeDao recipeDao = new RecipeDaoImpl();
-        Recipe recipe = recipeDao.loadRecipe(Long.parseLong(id));
-        if(recipe != null) {
-            review.setRecipe(recipe);
-            reviewDao.saveNewReview(review);
-            resp.setStatus(HttpServletResponse.SC_OK);
-        } else {
-            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-        }
-
-
-    }
 
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
@@ -49,11 +33,48 @@ public class SingleReviewServlet extends HttpServlet {
 
     }
 
-    private Review createReview(HttpServletRequest req) {
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String reviewId = req.getPathInfo();
+        Long id = null;
+        try {
+            id = Long.parseLong(reviewId.substring(1));
+        } catch (NumberFormatException e) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
 
-        int rating = Integer.parseInt(req.getParameter("rating"));
-        String opinion = req.getParameter("opinion");
-        String author = req.getParameter("author");
-        return new Review(rating, opinion, author);
+        BufferedReader br = new BufferedReader(new InputStreamReader(req.getInputStream()));
+        String data = br.readLine();
+
+        Map<String, String> parsedData = parseFromData(data);
+        int rating = Integer.parseInt(parsedData.get("rating"));
+        String opinion = parsedData.get("opinion");
+        String author = parsedData.get("author");
+
+        if (reviewDao.updateReview(rating, opinion, author, id)) {
+            resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+        } else {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
     }
+
+    private Map<String,String> parseFromData(String data) {
+
+        Map<String, String> parameters = new HashMap<>();
+        String[] pairs = data.split("&");
+        for (String pair : pairs) {
+            String[] keyValue = pair.split("=");
+            String value = null;
+            try {
+                value = new URLDecoder().decode(keyValue[1], "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            parameters.put(keyValue[0], value);
+        }
+        return parameters;
+    }
+
 }
+
+
+
